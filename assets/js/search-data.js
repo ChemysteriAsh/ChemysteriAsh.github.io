632b3e7ecb41c99af01ad6b1006e330fea2b14dd
ChemysteriAsh <56759570+ChemysteriAsh@users.noreply.github.com>
2026-05-17 05:33:12 +0000

    Deploying to gh-pages from @ ChemysteriAsh/ChemysteriAsh.github.io@fad2b689bb486654d1d53fa2123bf9a24c26bdaa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
+        },{id: "nav-publications",
+          title: "publications",
+          description: "A collection of my research and publications.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
+          },
         },{id: "nav-projects",
           title: "projects",
           description: "A growing collection of your cool projects.",
